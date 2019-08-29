4616c3bfb57ac2c2f0466e4209422ece8ec48e81
Abbas <abbask.51tr@gmail.com>
2019-08-29 20:05:46 +0530

    Added connection to sql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const port = 5000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -12,3 +13,25 @@ app.get("/getData", (req, res) => {
 app.get("/", (req, res) => res.send("Welcome to the Home Page"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : '',
+  password        : 'abbastra51',
+  database        : 'aa-dbms'
+});
+
+pool.getConnection(function(err, connection) {
+  if (err) throw err; 
+
+  connection.query('SELECT * FROM products', function (error, results, fields) {
+
+    console.log(results)
+    connection.release();
+
+    if (error) throw error;
+
+  });
+});
