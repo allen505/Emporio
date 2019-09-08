c30847d6bc36e18d2a542811df129c9a9b1178ad
healthyCoder <allenabraham505@gmail.com>
2019-09-08 15:43:41 +0530

    Added react router and added a router link for Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 import {
 	Menu,
 	Icon,
@@ -14,6 +16,8 @@ import {
 } from "antd";
 import "./App.css";
 
+import WrappedLogin from "./pages/Login";
+
 const { Title } = Typography;
 const { Header, Content, Footer } = Layout;
 
@@ -26,129 +30,23 @@ class App extends React.Component {
 			data: "Nothing recieved"
 		};
 	}
-	componentDidMount() {
-		this.getData();
-	}
-
-	getData = () => {
-		fetch("/getData")
-			.then(res => res.json())
-			.then(newData => {
-				this.setState({ data: newData });
-				console.log("The new data is: " + newData);
-			})
-			.catch(e => {
-				console.log(e);
-			});
-	};
-
-	handleSubmit = e => {
-		e.preventDefault();
-		this.props.form.validateFields((err, values) => {
-			if (!err) {
-				console.log("Received values of form: ", values);
-			}
-		});
-	};
 
 	render() {
-		let { data } = this.state;
-
-		const { getFieldDecorator } = this.props.form;
-
 		return (
-			<Layout className="layout" style={{ minHeight: "100vh" }}>
-				<Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-					<Menu
-						theme="dark"
-						mode="horizontal"
-						defaultSelectedKeys={["2"]}
-						style={{ lineHeight: "64px" }}
-					>
-						<Menu.Item key="1">Brand Name</Menu.Item>
-					</Menu>
-				</Header>
-
-				<Content
-					style={{
-						padding: "75px 50px",
-						marginTop: 64,
-						textAlign: "center"
-					}}
-				>
-					<Row type="flex" align="center">
-						<Col
-							span={8}
-							style={{ padding: "60px", backgroundColor: "#ffffff", borderRadius: "15px" }}
-						>
-							<Form onSubmit={this.handleSubmit} className="login-form">
-								<Form.Item>
-									{getFieldDecorator("username", {
-										rules: [
-											{
-												required: true,
-												message: "Please input your username!"
-											}
-										]
-									})(
-										<Input
-											prefix={
-												<Icon
-													type="user"
-													style={{ color: "rgba(0,0,0,.25)" }}
-												/>
-											}
-											placeholder="Username"
-										/>
-									)}
-								</Form.Item>
-								<Form.Item>
-									{getFieldDecorator("password", {
-										rules: [
-											{
-												required: true,
-												message: "Please input your Password!"
-											}
-										]
-									})(
-										<Input
-											prefix={
-												<Icon
-													type="lock"
-													style={{ color: "rgba(0,0,0,.25)" }}
-												/>
-											}
-											type="password"
-											placeholder="Password"
-										/>
-									)}
-								</Form.Item>
-								<Form.Item>
-									<Button
-										type="primary"
-										htmlType="submit"
-										className="login-form-button"
-									>
-										Log in
-									</Button>
-									<br />
-									Or <a href="">register now!</a>
-								</Form.Item>
-							</Form>
-						</Col>
-					</Row>
-				</Content>
-				<Footer style={{ textAlign: "center" }}>
-					This project is created by Allen and Abbas
-				</Footer>
-			</Layout>
+			<Router>
+				<div>
+					<ul>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+					</ul>
+					<Switch>
+						<Route path="/login" component={WrappedLogin} />
+					</Switch>
+				</div>
+			</Router>
 		);
 	}
 }
 
-// export default App;
-
-const WrappedLogin = Form.create()(App);
-ReactDom.render(<WrappedLogin />, document.getElementById("root"));
-
-export default WrappedLogin;
+export default App;
