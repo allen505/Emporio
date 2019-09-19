32d879b1a0f8cf703364d376f16265048c698e0f
Abbas <abbask.51tr@gmail.com>
2019-09-19 18:46:54 +0530

    Search bar added

--- client/src/pages/Navigator.js
+++ client/src/pages/Navigator.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Menu } from "antd";
+import { Menu,Input } from "antd";
 import "./Login.css";
 
 const Nav = () => {
+	const { Search } = Input;
 	return (
 			<Menu
 				theme="dark"
@@ -11,6 +12,14 @@ const Nav = () => {
 				style={{ lineHeight: "64px" }}
 			>
 				<Menu.Item key="1">Brand Name</Menu.Item>
+				<Search
+                  placeholder="Search for the items"
+                  onSearch={value => console.log(value)}
+                  style={{ width: 1150 , margin:"20px"}}
+                />
+				<Menu.Item key="2">Home</Menu.Item>
+				<Menu.Item key="3">Cart</Menu.Item>
+				<Menu.Item key="4">User</Menu.Item>
 			</Menu>
 
 	);
