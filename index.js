4a4caa2b4cc2d60070ff09ae1f581d3414f3155a
healthyCoder <allenabraham505@gmail.com>
2019-09-28 12:23:08 +0530

    Passwords are now hashed before storage Size of password column in database is increased from 20 to 70

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const app = express();
 
 const port = 5000;
@@ -24,30 +25,35 @@ let pool = mysql.createPool({
 });
 
 app.post("/api/login", jsonParser, (req, res) => {
-	let dets = req.body;
+	let requestData = req.body;
+
 	pool.getConnection((err, connection) => {
 		if (err) {
 			console.log("Error in getting connection");
 		} else {
 			connection.query(
 				"SELECT Lid,Password FROM login",
-				(error, result, fields) => {
+				(error, result, fields) => {					
+					connection.release();
 					let i = 0;
 					let valid = false;
-
 					for (i = 0; i < result.length; i++) {
-						if (result[i].Lid == req.body.username) {
+						if (result[i].Lid == requestData.username) {
 							valid = true;
 							break;
 						}
 					}
 					if (valid == true) {
-						if (result[i].Password != req.body.password) {
-							valid = false;
-						}
+						bcrypt
+							.compare(requestData.password, result[i].Password)
+							.then(passRes => {
+								valid = passRes;
+								res.json(valid);
+							});
 					}
-					res.json(valid);
-					connection.release();
+					else{
+						res.json(valid);
+					}
 				}
 			);
 		}
@@ -57,13 +63,38 @@ app.post("/api/login", jsonParser, (req, res) => {
 app.get("/", (req, res) => res.send("Welcome to the Home Page"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+let hashed = "";
+
+const testHash = () => {
+	let myPlaintextPassword = "admin";
+	let saltRounds = 5;
+
+	bcrypt.hash(myPlaintextPassword, saltRounds).then(hash => {
+		// Store hash in your password DB.
+		console.log(myPlaintextPassword);
+		console.log(hash);
+		hashed = hash;
+		bcrypt.hash(myPlaintextPassword, saltRounds).then(hash2 => {
+			console.log(myPlaintextPassword);
+			console.log(hash2);
+			bcrypt.compare(myPlaintextPassword, hash).then(res => {
+				console.log(res);
+				bcrypt.compare(myPlaintextPassword, hash2).then(secres => {
+					console.log(secres);
+				});
+			});
+		});
+	});
+};
+
+// testHash();
+
 pool.getConnection((err, connection) => {
 	if (err) throw err;
-
 	connection.query(
-		"SELECT Lid,Password FROM login",
+		"SELECT Lid,Password,Type FROM login",
 		(error, results, fields) => {
-			// console.log(results)
+			// console.log(results[0].Lid);
 			connection.release();
 			if (error) throw error;
 		}
