d723c01024b81edb4aa2fe32c16e3869887e3b97
Abbas <abbask.51tr@gmail.com>
2019-11-08 10:54:48 +0530

    added success screen

--- client/src/pages/Cards.js
+++ client/src/pages/Cards.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Card, Icon, Button } from "antd";
+import { Card, Icon, Button, Result, Popover, Modal, Alert } from "antd";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
 class Cards extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			loading : true,
-			userid : null,
+			loading: true,
+			userid: null,
+			visible: false,
+			product: null
 		}
-		try{
+		try {
 			this.state.userid = props.id;
 			let sid = props.product.Sid;
 			console.log(sid)
@@ -16,14 +19,22 @@ class Cards extends React.Component {
 			console.log(e);
 		}
 	}
-	buyproduct = (Sid,e) =>{
-		let order=
-			{
-				pid : e.target.value,
-				bid : this.state.userid,
-				sid : Sid,
-				price : 1000
-			}
+
+	success = (Pname) => {
+		this.setState({
+			visible: true,
+			product:Pname
+		})
+	}
+
+	buyproduct = (Sid, e, Pname) => {
+		let order =
+		{
+			pid: e.target.value,
+			bid: this.state.userid,
+			sid: Sid,
+			price: 1000
+		}
 		let register = () => {
 			fetch("/api/orders", {
 				method: "POST",
@@ -32,11 +43,10 @@ class Cards extends React.Component {
 				},
 				body: JSON.stringify(order)
 			})
-				// .then(res => res.json())
+				.then(res => res.json())
 				.then(resp => {
-					console.log(resp);
 					if (resp) {
-						this.success();
+						this.success(Pname);
 					} else {
 						this.error();
 					}
@@ -44,32 +54,50 @@ class Cards extends React.Component {
 		};
 		register();
 	}
+	handleCancel=()=>{
+		this.setState({
+			visible:false
+		})
+	}
 	render() {
-		setTimeout(() => { 
-			this.setState(() => 
-			({
-				loading: false
-			}))
-		  }, 1000);
+		setTimeout(() => {
+			this.setState(() =>
+				({
+					loading: false
+				}))
+		}, 1000);
 		const { Meta } = Card;
 		var cardList = this.props.product.map(prod => {
 			return (
 				<div style=
-				{{
-					float:'left', 
-					width:'25%'
-				}}>
-				<Card loading={this.state.loading}
-					style={{ width: '90%' }}
-					cover={<img alt="example" src="" />}
-					actions={[
-						<Button type="primary" block onClick={(e)=>{this.buyproduct(prod.Sid,e)}} value = {prod.Pid}>
-							Buy
+					{{
+						float: 'left',
+						width: '25%'
+					}}>
+					<Card loading={this.state.loading}
+						style={{ width: '90%' }}
+						cover={<img alt="example" src="" />}
+						actions={[
+							<Button type="primary" block onClick={(e) => { this.buyproduct(prod.Sid, e, prod.Pname) }} value={prod.Pid}>
+								Buy
 						</Button>
-					]}
-				>
-					<Meta title={prod.Pname} description={prod.Category} />
-				</Card>
+						]}
+					>
+						<Meta title={prod.Pname} description={prod.Category} />
+					</Card>
+					<Modal
+						title="Basic Modal"
+						visible={this.state.visible}
+						onOk={this.handleOk}
+						footer={[
+							<Button key="buy" onClick={this.handleCancel}>Back to shopping</Button>]}
+						onCancel={this.handleCancel}
+					  ><Result
+					  status="success"
+					  title={"Successfully Purchased  "+ this.state.product}
+					/>
+					  </Modal>
+						
 				</div>
 			);
 		});
